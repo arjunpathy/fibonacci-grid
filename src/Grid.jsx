c2540221c039dff158209f7c52fce25b694c9374
Arjun <nagarjun2695@gmail.com>
2024-10-23 16:22:39 +0200

    code clean up

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import Cell from "./Cell";
 import "./Grid.css";
 
-const [ROWS, COLS] = [20, 20];
+const GRID_LENGTH = 50;
 const FIBONACCI_SEQUENCE = [1, 1, 2, 3, 5];
 const len = FIBONACCI_SEQUENCE.length;
 
 const Grid = () => {
   const [grid, setGrid] = useState(
-    Array.from({ length: ROWS }, () => Array(COLS).fill(0))
+    Array.from({ length: GRID_LENGTH }, () => Array(GRID_LENGTH).fill(0))
   );
   const [highlightedCells, setHighlightedCells] = useState([]);
   const [fibonacciCells, setFibonacciCells] = useState([]);
@@ -17,7 +17,7 @@ const Grid = () => {
     const newGrid = grid.map((row) => [...row]);
     const cellsToHighlight = [];
 
-    for (let i = 0; i < ROWS; i++) {
+    for (let i = 0; i < GRID_LENGTH; i++) {
       newGrid[row][i] = newGrid[row][i] ? newGrid[row][i] + 1 : 1;
       newGrid[i][col] = newGrid[i][col] ? newGrid[i][col] + 1 : 1;
       cellsToHighlight.push([row, i]);
@@ -36,47 +36,46 @@ const Grid = () => {
   const checkAndClearFibonacci = (newGrid) => {
     let fibo = [];
 
-    for (let i = 0; i < ROWS; i++) {
-      for (let j = 0; j < COLS; j++) {
+    for (let i = 0; i < GRID_LENGTH; i++) {
+      for (let j = 0; j < GRID_LENGTH; j++) {
 
         if (newGrid[i][j] === FIBONACCI_SEQUENCE[len - 1]) {                    // check for fibonacci only if the last fibonacci number is present
 
+          // Left to Right"
           if (j >= len - 1) {
             let sequenceArray = newGrid[i].slice(j - len + 1, j + 1);
-            if (sequenceArray.every((ele, index) => ele === FIBONACCI_SEQUENCE[index])) {
-              console.log("Left to Right");
+            if (isFibonacci(sequenceArray)) 
               fibo.push(...sequenceArray.map((_, idx) => [i, j - len + 1 + idx]));
-            }
           }
 
-          if (j <= ROWS - len) {
+          // "Right to Left"
+          if (j <= GRID_LENGTH - len) {
             let sequenceArray = newGrid[i].slice(j, j + len);
             sequenceArray = sequenceArray.reverse();
-            if (sequenceArray.every((ele, index) => ele === FIBONACCI_SEQUENCE[index])) {
-              console.log("Right to Left");
+            if (isFibonacci(sequenceArray)) 
               fibo.push(...sequenceArray.map((_, idx) => [i, j + idx]));
-            }
           }
 
+          // "Top to Bottom"
           if (i >= len - 1) {
             let sequenceArray = newGrid.slice(i - len + 1, i + 1).map((r) => r[j]);
-            if (sequenceArray.every((ele, index) => ele === FIBONACCI_SEQUENCE[index])) {
-              console.log("Top to Bottom");
+            if (isFibonacci(sequenceArray)) 
               fibo.push(...sequenceArray.map((_, idx) => [i - len + 1 + idx, j]));
-            }
           }
-          if (i <= ROWS - len) {
+          
+          // "Bottom to Top"
+          if (i <= GRID_LENGTH - len) {
             let sequenceArray = newGrid.slice(i, i + len).map((r) => r[j]);
             sequenceArray = sequenceArray.reverse();
-            if (sequenceArray.every((ele, index) => ele === FIBONACCI_SEQUENCE[index])) {
-              console.log("Bottom to Top");
+            if (isFibonacci(sequenceArray)) 
               fibo.push(...sequenceArray.map((_, idx) => [i + idx, j]));
-            }
           }
+
           if (fibo.length) {
             setFibonacciCells([...fibo]);
             setTimeout(() => clearFibonacci(newGrid, fibo), 1000);
           }
+
         }
       }
     }
@@ -91,10 +90,12 @@ const Grid = () => {
     setFibonacciCells([]);
   };
 
+  const isFibonacci = (arr) =>{return  arr.every((ele, index) => ele === FIBONACCI_SEQUENCE[index]);}
+
   return (
     <div
       className="grid"
-      style={{ gridTemplateColumns: `repeat(${COLS}, 20px)` }}
+      style={{ gridTemplateColumns: `repeat(${GRID_LENGTH}, 20px)` }}
     >
       {grid.map((row, rowIndex) =>
         row.map((value, colIndex) => {
